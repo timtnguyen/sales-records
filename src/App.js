e2040b7d58adc997f9943aa0a7411ff40973733b
Thinh Nguyen <nguyen710150@yahoo.com>
2019-11-19 15:13:15 -0800

    store users in firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { auth } from "./firebase/firebase.utils.js";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 import HomePage from "./pages/home-page/home-page.component";
 import NavBar from "./components/navbar/navbar.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -18,8 +18,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-buttons/custom-button.component";
+import {
+  auth,
+  createUserProfileDocument
+} from "../../firebase/firebase.utils.js";
 
 import "./sign-up.styles.scss";
 
@@ -14,8 +20,90 @@ class SignUp extends React.Component {
     };
   }
 
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("password dont't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
-    return <div>Sign up form</div>;
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="page">
+        <div className="sign-up">
+          <h2 className="title">I do not have an account</h2>
+          <span>Sign up with your email and password</span>
+
+          <form className="sign-up-form" onSubmit={this.handleSubmit}>
+            <FormInput
+              type="text"
+              name="displayName"
+              value={displayName}
+              placeholder="Display Name"
+              onChange={this.handleChange}
+              label="Display Name"
+              required
+            />
+            <FormInput
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Email"
+              onChange={this.handleChange}
+              label="Email"
+              required
+            />
+            <FormInput
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={this.handleChange}
+              label="Password"
+              required
+            />
+            <FormInput
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={this.handleChange}
+              label="Confirm Password"
+              required
+            />
+
+            <CustomButton type="submit">SIGN UP</CustomButton>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
